4dbf683fe9af25f457049d40e6304a4eb064a8e3
shivasai bonala <48891181+Lord-fourth@users.noreply.github.com>
2021-06-06 00:38:21 +0530

    added tabs and lifted state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import NewTask from "./components/newTask";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
+  const [listOfTasks, setListOfTasks] = useState([]);
+  const [newTask, setNewTask] = useState("I wanna ");
+  const [defaultValue, setDefaultValue] = useState("I wanna ");
+  const handleKeyPress = (e) => {
+    if (e.charCode === 13) {
+      addNewTask();
+    }
+  };
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+    // this.setState({ newTask: event.target.value });
+  };
+  function addNewTask() {
+    console.log("inside addnewtask");
+    console.log(listOfTasks);
+    console.log("newTask:", newTask);
+    setListOfTasks([
+      ...listOfTasks,
+      {
+        id: listOfTasks.length + 1,
+        value: newTask,
+      },
+    ]);
+    setNewTask(defaultValue);
+  }
+
   return (
-    <div className="App">
-      <NewTask />
-    </div>
+    <React.Fragment>
+      <h1>To Do App</h1>
+      <Tabs>
+        <TabList>
+          <Tab>All</Tab>
+          <Tab>Completed</Tab>
+        </TabList>
+
+        <TabPanel>
+          <NewTask
+            listOfTasks={listOfTasks}
+            newTask={newTask}
+            defaultValue={defaultValue}
+            addTask={addNewTask}
+            handleKeyPress={handleKeyPress}
+            handleChange={handleChange}
+          />
+        </TabPanel>
+        <TabPanel>
+          <NewTask
+            listOfTasks={listOfTasks}
+            newTask={newTask}
+            defaultValue={defaultValue}
+            addTask={addNewTask}
+            handleKeyPress={handleKeyPress}
+            handleChange={handleChange}
+          />
+        </TabPanel>
+      </Tabs>
+    </React.Fragment>
   );
 }
 
--- src/components/newTask.jsx
+++ src/components/newTask.jsx
@@ -1,49 +1,45 @@
-import React, { Component } from "react";
-class NewTask extends Component {
-  state = {
-    listOfTasks: [],
-    newTask: "I wanna ",
-    defaultValue: "I wanna ",
-  };
-  handleKeyPress = (e) => {
-    if (e.charCode === 13) {
-      this.addTask();
-    }
-  };
-  handleChange = (event) => {
-    this.setState({ newTask: event.target.value });
-  };
-  addTask = () => {
-    const listOfTasks = [
-      ...this.state.listOfTasks,
-      { id: this.state.listOfTasks.length + 1, value: this.state.newTask },
-    ];
+import React from "react";
 
-    const newTask = this.state.defaultValue;
-    this.setState({ listOfTasks });
-    this.setState({ newTask });
-  };
-  render() {
-    return (
-      <React.Fragment>
-        <input
-          type="text"
-          name="defaultValue"
-          value={this.state.newTask}
-          onChange={this.handleChange}
-          onKeyPress={this.handleKeyPress.bind(this)}
-        />
-        <button onClick={this.addTask} className="btn btn-primary btn-sm m-2">
-          Add
-        </button>
-        <div>
-          {this.state.listOfTasks.map((txt) => (
-            <li key={txt.id}>{txt.value}</li>
-          ))}
-        </div>
-      </React.Fragment>
-    );
-  }
+function NewTask(props) {
+  // const handleKeyPress = (e) => {
+  //   if (e.charCode === 13) {
+  //     this.addTask();
+  //   }
+  // };
+  // const handleChange = (event) => {
+  //   this.setState({ newTask: event.target.value });
+  // };
+  // addTask = () => {
+  //   const listOfTasks = [
+  //     ...this.state.listOfTasks,
+  //     { id: this.state.listOfTasks.length + 1, value: this.state.newTask },
+  //   ];
+
+  //   const newTask = this.state.defaultValue;
+  //   this.setState({ listOfTasks });
+  //   this.setState({ newTask });
+  // };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        name="defaultValue"
+        value={props.newTask}
+        onChange={props.handleChange}
+        onKeyPress={props.handleKeyPress}
+      />
+
+      <button onClick={props.addTask} className="btn btn-primary btn-sm m-2">
+        Add
+      </button>
+      <div>
+        {props.listOfTasks.map((txt) => (
+          <li key={txt.id}>{txt.value}</li>
+        ))}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default NewTask;
